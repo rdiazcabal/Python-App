ec60a76bcb12dd235d7e4407d5282c8ab55f4dde
rdiazcabal <rdiazcabal1980@gmail.com>
2023-10-30 10:32:02 -0600

    none

---
+++ function_app.py
@@ -0,0 +1,20 @@
+import logging
+import azure.functions as func
+import requests
+from bs4 import BeautifulSoup
+
+def revisar_linea_de_texto(req):
+    url = "https://www.simplifiedlogistics.com/heartbeattest/heartBeatReport.aspx"
+    text_object = "HeartBeat Status Good"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        contein = soup.get_text()
+
+        if text_object in contein:
+            return f"The Line '{text_object}' Was found Successfully in the page."
+        else:
+            return f"The Line '{text_object}' Was Not found in the page."
+    else:
+        return f"The web page could not be accessed. Status code: {response.status_code}"
